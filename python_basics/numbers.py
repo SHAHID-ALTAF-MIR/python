2f6278dc0e833032931ae49dd384e2328a5c5e7d
SHAHID-ALTAF-MIR <shahidmeer912@gmail.com>
2026-04-18 22:30:49 +0530

    Create numbers.py

---
+++ python_basics/numbers.py
@@ -0,0 +1,12 @@
+x = 1
+x = 1.1
+x = 1+2j
+y = 23
+print(10+2)  # addition
+print(12-3)  # subtraction
+print(3*3)  # multiplication
+print(222/222)  # division
+print(3//2)  # division with approx
+print(10 % 3)  # reminder
+print(10**8)  # exponential
+print(x*y)
