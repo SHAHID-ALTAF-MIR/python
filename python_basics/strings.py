b3ce609546b58c823a8afbcbdc829eb270558ff4
SHAHID-ALTAF-MIR <shahidmeer912@gmail.com>
2026-04-21 20:39:16 +0530

    Create strings.py

---
+++ python_basics/strings.py
@@ -0,0 +1,33 @@
+msg = """   hey
+hello 
+my 
+name 
+is 
+shahid
+altaf 
+mir
+"""
+print(msg)
+print(len(msg))
+print(msg[1])
+print(msg[2])
+print(msg[0:11])
+print("aslamu\"alaikum")
+# comment
+# \"
+# \\
+# \n   new line
+# full = first+""+last
+# print(full)
+# full=f"({len(first)}{last}"
+# print(full)
+# len()    function for length
+# [] for accessing any alphabit in the string
+print(msg.upper())
+print(msg.title())
+print(msg.strip())
+print(msg.lstrip())
+print(msg.find("shahid"))
+print("mir" in msg)
+print(msg.replace("shahid", "tawseef"))
+print("meer" not in msg)
