5147990c32db0818c50aacb17b4a9ace00343e5d
SHAHID-ALTAF-MIR <shahidmeer912@gmail.com>
2026-04-22 21:29:04 +0530

    Create conditional_statements.py

---
+++ python_basics/conditional_statements.py
@@ -0,0 +1,9 @@
+temp = 25
+if temp > 30:
+    print("turn the ac on !!!!")
+elif temp < 25:
+    print("turn the the heater on")
+elif temp == 25:
+    print(" room temp is perfect")
+else:
+    print("room temp is great")
