f13b6fe3a2a12aa1bc01bf1c2c1577348f57f5c3
SHAHID-ALTAF-MIR <shahidmeer912@gmail.com>
2026-04-26 23:40:42 +0530

    Create working_with_numbers.py

---
+++ python_basics/working_with_numbers.py
@@ -0,0 +1,8 @@
+import math
+print(round(5.8)) # round off
+print(abs(-32)) #takes modulus of a number
+print(math.ceil(3.453))# to get the ceil/upper number
+print(math.factorial(24)) #gives factorial of a number
+print(math.inf)#positve infinity
+print(math.pi)#value of pi
+print(math.gamma(4))# special function gamma
