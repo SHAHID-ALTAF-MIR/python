994768b083c34448f3ada73d6672000e1dca394c
SHAHID-ALTAF-MIR <shahidmeer912@gmail.com>
2026-05-14 23:49:54 +0530

    Add dict comprehension example in dict_comprehension.py

---
+++ what_i_learnt_today/dict_comprehension.py
@@ -0,0 +1,10 @@
+# dict comprehensions
+
+stat = [("age", 21), ("weight", 70), ("height", 180), (("D0B", 2005))]
+print(stat)
+
+dict_stat = {key: value for (key, value) in stat}
+print(dict_stat)
+
+
+# enumerate funtion :: if we wrap  a list in the enumerate function it will give us its index of the element and the element itself
