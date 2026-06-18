81442835b1019b2b5bc742799ff7bdc5c0944bc3
SHAHID-ALTAF-MIR <shahidmeer912@gmail.com>
2026-06-18 21:04:29 +0530

    Python Program to Print the Fibonacci sequence

---
+++ what_i_learnt_today/practice_set_8.py
@@ -0,0 +1,14 @@
+#  Python Program to Print the Fibonacci sequence
+def fibonacci(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a+b
+
+
+fibonacci(int(input("enter a number!  ")))
+
+# Python Program to Check Armstrong Number
+
+# Python Program to Find HCF or GCD
